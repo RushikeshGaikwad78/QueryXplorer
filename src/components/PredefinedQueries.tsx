f3fa6e1ae3baa60ca257a37b85fc17e5754a7afc
RushikeshGaikwad78 <rushigaikwad7898@gmail.com>
2025-03-31 10:45:50 +0530

    toggle functionality for test queries with proper list structure

--- src/components/PredefinedQueries.tsx
+++ src/components/PredefinedQueries.tsx
@@ -7,9 +7,11 @@ import {
   ListItemText,
   Typography,
   Divider,
+  Collapse,
 } from '@mui/material';
 import {
   QueryStats as QueryStatsIcon,
+  ExpandMore as ExpandMoreIcon,
 } from '@mui/icons-material';
 import { Query } from '../types';
 
@@ -27,34 +29,59 @@ const PredefinedQueries: React.FC<PredefinedQueriesProps> = ({ queries, onQueryS
 
   return (
     <Box sx={{ width: '100%' }}>
-      <Box sx={{ display: 'flex', alignItems: 'center', gap: 1, p: 1 }}>
-        <QueryStatsIcon />
-        <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
-          Test Queries
-        </Typography>
+      {/* Header with Clickable Icon */}
+      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', p: 1 }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+          <QueryStatsIcon />
+          <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+            Test Queries
+          </Typography>
+        </Box>
+        
+        {/* Clickable ExpandMore Icon */}
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            cursor: 'pointer',
+          }}
+          onClick={handleClick}
+          role="button"
+          aria-label="Toggle Test Queries"
+        >
+          <ExpandMoreIcon 
+            sx={{ 
+              transform: open ? 'rotate(180deg)' : 'rotate(0deg)', 
+              transition: '0.3s' 
+            }} 
+          />
+        </Box>
       </Box>
-      
-          <List disablePadding>
-            {queries.map((query) => (
-              <ListItem key={query.id} disablePadding>
-                <ListItemButton
-                  onClick={() => onQuerySelect(query)}
-                  sx={{ pl: 4 }}
-                >
-                  <ListItemText
-                    primary={query.name}
-                    secondary={query.description}
-                    primaryTypographyProps={{ variant: 'body2' }}
-                    secondaryTypographyProps={{ variant: 'caption' }}
-                  />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-       
+
+      {/* Collapsible Query List */}
+      <Collapse in={open}>
+        <List disablePadding>
+          {queries.map((query) => (
+            <ListItem key={query.id} disablePadding>
+              <ListItemButton
+                onClick={() => onQuerySelect(query)}
+                sx={{ pl: 4 }}
+              >
+                <ListItemText
+                  primary={query.name}
+                  secondary={query.description}
+                  primaryTypographyProps={{ variant: 'body2' }}
+                  secondaryTypographyProps={{ variant: 'caption' }}
+                />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Collapse>
+
       <Divider />
     </Box>
   );
 };
 
-export default PredefinedQueries; 
+export default PredefinedQueries;

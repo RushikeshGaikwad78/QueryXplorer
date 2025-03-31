22a8b1fc4487a35d128126a09d7b10a6131b35f7
RushikeshGaikwad78 <rushigaikwad7898@gmail.com>
2025-03-31 13:58:49 +0530

    test query description

--- src/data/preDefinedQueries.ts
+++ src/data/preDefinedQueries.ts
@@ -4,35 +4,35 @@ export const predefinedQueries: Query[] = [
         id: 'products',
         name: 'List All Products',
         sql: 'SELECT * FROM products',
-        description: 'Display all products from the database',
+        description: 'SELECT * FROM products',
         table: 'products.csv'
       },
       {
         id: 'orders',
         name: 'List All Orders',
         sql: 'SELECT * FROM orders',
-        description: 'Show all orders',
+        description: 'SELECT * FROM orders',
         table: 'orders.csv'
       },
       {
         id: 'order_details',
         name: 'Order Details',
         sql: 'SELECT * FROM order_details',
-        description: 'Display detailed order information',
+        description: 'SELECT * FROM order_details',
         table: 'order_details.csv'
       },
       {
         id: 'suppliers',
         name: 'List Suppliers',
         sql: 'SELECT * FROM suppliers',
-        description: 'Show all suppliers',
+        description: 'SELECT * FROM suppliers',
         table: 'suppliers.csv'
       },
       {
         id: 'shippers',
         name: 'List Shippers',
         sql: 'SELECT * FROM shippers',
-        description: 'Display all shipping companies',
+        description: 'SELECT * FROM shippers',
         table: 'shippers.csv'
       }
   ];
